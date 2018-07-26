4783f194ad9659d982ca4b7eb7fc2bc804451898
M Haidar Hanif <git@mhaidarhanif.com>
2018-07-26 10:47:28 +0700

    Fix issues on sync between variable and storage

--- index.js
+++ index.js
@@ -1,58 +1,73 @@
+'use strict';
+
 const formAdd = document.getElementById('form-add')
-let dataPeoples = [] //Set empty array to handle user data input
+const resultField = document.getElementById('result-field');
 
-//Form validation [Bootstrap 4]
-// (function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        var forms = document.getElementsByClassName('needs-validation');
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                event.preventDefault()
-                if (form.checkValidity() === false) {
-                    event.stopPropagation();
-                }else{
-                    addToDOM()
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-// })();
+const get = () => {
+    // Get data from storage, or set empty data
+    const PEOPLE = window.localStorage.getItem('PEOPLE')
 
+    if (PEOPLE) return JSON.parse(PEOPLE)
+    else return []
+}
 
+const set = (newData) => {
+    window.localStorage.setItem('PEOPLE', JSON.stringify(newData))
+}
 
-//Function to call the data from local data storage
-const showPeoples = () =>{
-    const localStoragePeoples = window.localStorage.getItem('peoples')
-    dataPeoples = JSON.parse(localStoragePeoples) || []
-    dataPeoples.forEach(people => {
-        
-        let divChild = document.createElement('div')
-        let divChildInner = '';
-        for (var key in people) {
-            divChildInner += `${key}: ${people[key]}<br>`
+let PEOPLE = get()
+
+// Form validation [Bootstrap 4]
+
+window.addEventListener('load', function () {
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault()
+            if (form.checkValidity() === false) {
+                event.stopPropagation();
+            } else {
+                addToStorage()
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}, false);
+
+// Function to call the data from local data storage
+const showPeople = () => {
+    const PEOPLE_STORAGE = window.localStorage.getItem('PEOPLE');
+    const PEOPLE = JSON.parse(PEOPLE_STORAGE) || [];
+
+    resultField.innerHTML = ""
+
+    PEOPLE.forEach(person => {
+        const divChild = document.createElement('div')
+        let personInfo = '';
+
+        for (let key in person) {
+            personInfo += `${key}: ${person[key]}<br>`
         }
-        divChild.innerHTML = `<b>People</b><br> ${divChildInner}`
-        let div = document.getElementById('result-field');
-        div.appendChild(divChild)  
+
+        divChild.innerHTML = `<b>Person:</b><br> ${personInfo}`
+
+        resultField.appendChild(divChild)
     })
 }
 
-//DOM Function 
-const addToDOM = () => {
-    const peoples = {
-        firstname: document.getElementById('firstname').value,
-        lastname: document.getElementById('lastname').value,
+// DOM Function 
+const addToStorage = () => {
+    const newPerson = {
+        firstName: document.getElementById('first-name').value,
+        lastName: document.getElementById('last-name').value,
         address: document.getElementById('address').value,
         phoneNumber: document.getElementById('phone-number').value,
         email: document.getElementById('email').value
     }
-    dataPeoples.push(peoples)
-    const result= JSON.stringify(dataPeoples)
-    
-    // console.log(dataPeoples)
-    window.localStorage.setItem('peoples',result)
-    showPeoples()    
+
+    PEOPLE.push(newPerson)
+    set(PEOPLE)
+    showPeople()
 }
-showPeoples()
+
+showPeople()
